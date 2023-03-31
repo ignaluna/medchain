45a50f4545ccde85ffdbddbc797518b73be4daec
Igna Luna <ignaluna98pb@gmail.com>
2023-03-31 15:39:50 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Cedalio ⨯ To-Do dApp Multi Data Base Example
+
+[Join our Discord Community](https://discord.gg/kSdhmb9UUT)
+
+[![Deploy to Cedalio](https://cedalio.com/images/deploy-schema-button-small.png)](https://docs.cedalio.com/introduction/getting-started/download-the-cli)
+</br>
+
+## Getting Started
+
+1. Run `npm install` at the root directory
+2. Create a `.env.development` with this variables.
+```
+REACT_APP_WC_PROJECT_ID=WALLET-CONNECT-ID
+GENERATE_SOURCEMAP=false
+REACT_APP_GRAPHQL_GATEWAY_BASE_URL=https://gateway.staging.cedalio.io
+REACT_APP_NET_SCAN_BASE=https://mumbai.polygonscan.com/address/ (or your selected network scan)
+```
+3. Then run: `npm start`
+
+- Note that we are using our gateway to deploy and interact with the blockchain with graphql
+
+## Learn More About Cedalio
+
+To learn more about Cedalio, take a look at the following resources:
+
+- [Cedalio](https://cedalio.com/) - learn about Cedalio Features and Roadmap.
+
+- [Cedalio Docs](https://docs.cedalio.com/) - learn about Cedalio Architecture and CLI.
---
+++ src/App.tsx
@@ -0,0 +1,213 @@
+import React, { useEffect } from "react";
+import "./App.css"
+import {
+  EthereumClient,
+  modalConnectors,
+  walletConnectProvider,
+} from "@web3modal/ethereum";
+import { Web3Modal } from "@web3modal/react";
+import { configureChains, createClient, WagmiConfig, useAccount } from "wagmi";
+import { polygonMumbai } from "wagmi/chains";
+import { Web3Button } from "@web3modal/react";
+import axios from "axios";
+
+import Header from "./components/Header";
+import ListComponent from "./components/ListComponent";
+import Footer from "./components/Footer";
+
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import Typography from '@mui/material/Typography';
+import ReactGA from 'react-ga';
+import { Rings } from 'react-loader-spinner'
+
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #FF7D3A',
+  borderRadius: '10px',
+  boxShadow: 24,
+  p: 4,
+  display: "flex",
+  flexDirection: "column",
+};
+
+const chains = [polygonMumbai];
+
+const projectId = String(process.env.REACT_APP_WC_PROJECT_ID)
+const TRACKING_ID = String(process.env.REACT_APP_TRACKING_ID)
+
+// Wagmi client
+const { provider } = configureChains(chains, [
+  walletConnectProvider({ projectId: projectId }),
+]);
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors: modalConnectors({ appName: "web3Modal", chains }),
+  provider,
+});
+
+// Web3Modal Ethereum Client
+const ethereumClient = new EthereumClient(wagmiClient, chains);
+
+export default function App() {
+  const [deployed, setDeployed] = React.useState(false);
+  const [uri, setUri] = React.useState('');
+  const [contractAddress, setContractAddress] = React.useState<string | undefined>();
+  const projectId = String(process.env.REACT_APP_WC_PROJECT_ID)
+  const { address } = useAccount()
+  const [open, setOpen] = React.useState(false);
+  const [response, setResponse] = React.useState("");
+  const handleClose = () => setOpen(false);
+  
+  function requestDeployToGateway(address: string) {
+    const url = `${process.env.REACT_APP_GRAPHQL_GATEWAY_BASE_URL}/deploy`
+    const payload = {
+      email: "todo-multi.cedalio.com",
+      schema: `type Todo {
+            id: UUID!
+            title: String!
+            description: String
+            priority: Int!
+            owner: String!
+            tags: [String!]
+            status: String
+          }
+          
+          `,
+      schema_owner: address,
+      network: "polygon:mumbai"
+    }
+    setOpen(true)
+    axios.post(
+      url, payload
+    ).then(function (response: any) {
+      localStorage.setItem('deploymentId', response.data.deployment_id);
+      localStorage.setItem('contractAddress', response.data.contract_address);
+      localStorage.setItem('deployed', 'true');
+      setContractAddress(response.data.contract_address)
+      setDeployed(true)
+      setOpen(false)
+      setResponse("success")
+      setUri(`${String(process.env.REACT_APP_GRAPHQL_GATEWAY_BASE_URL)}/${response.data.deployment_id}/graphql`)
+    })
+      .catch(function (error: any) {
+        console.log(error);
+        setResponse("error")
+      })
+  }
+
+  function redeploy(){
+    setResponse("")
+    return requestDeployToGateway(String(address))
+  }
+
+  useEffect(() => {
+    const deployed = Boolean(localStorage.getItem('deployed'))
+    const contractAddress = localStorage.getItem('contractAddress')
+    const deploymentId = localStorage.getItem('deploymentId')
+    if (deployed && contractAddress && deploymentId) {
+      setUri(`${String(process.env.REACT_APP_GRAPHQL_GATEWAY_BASE_URL)}/${deploymentId}/graphql`)
+      setDeployed(deployed)
+      setContractAddress(contractAddress)
+    }
+    else if (address) {
+      requestDeployToGateway(address)
+    }
+    else {
+      return
+    }
+  }, [address])
+
+  const client = new ApolloClient({
+    uri,
+    cache: new InMemoryCache({
+      addTypename: false,
+    }),
+  });
+
+  ReactGA.initialize(TRACKING_ID);
+
+  const Loader = () => {
+    if(response === "error"){
+      return (
+          <div className="loader-layer" style={{justifyContent: 'center', display:"flex", flexDirection:"column"}}>
+              <p className='error-message' style={{textTransform:'uppercase', textAlign:"center", color:"black", fontWeight:"200"}}>We had a <strong> problem </strong>trying to deploy please <strong>retry</strong>.</p>
+              <button className="retry-button" onClick={redeploy}>RETRY</button>
+          </div>
+      )
+    }
+    else{
+      return (
+          <div className="loader-layer" style={{justifyContent: 'center'}}>
+              <Rings
+                  height="100"
+                  width="100"
+                  radius={2}
+                  color="#FF7D3A"
+                  ariaLabel="puff-loading"
+                  wrapperStyle={{}}
+                  wrapperClass=""
+                  visible={true}
+              />
+          </div>
+      )
+    }
+        
+    }
+
+  return (
+    <ApolloProvider client={client}>
+      <div className="App">
+        <WagmiConfig client={wagmiClient}>
+          <Header />
+          <div className={address ? `button-container connected` : `button-container`} >
+            <Web3Button />
+          </div>
+          <div className="gif-container" style={address ? { display: 'none' } : { display: 'flex' }} >
+            <div className="web">
+              <img className="gif" src="home-gif.gif" alt="explained gif" />
+            </div>
+          </div>
+          {deployed ? <ListComponent address={address} /> : null}
+          <Web3Modal
+            projectId={projectId}
+            ethereumClient={ethereumClient}
+          />
+          <Modal
+            aria-labelledby="transition-modal-title"
+            aria-describedby="transition-modal-description"
+            open={open}
+            onClose={handleClose}
+            closeAfterTransition
+            BackdropComponent={Backdrop}
+            BackdropProps={{
+              timeout: 500,
+            }}
+          >
+            <Fade in={open}>
+              <Box sx={style}>
+                <Typography id="transition-modal-title" variant="h6" component="h2" sx={{ fontWeight: "800", textAlign: "center" }}>
+                  Creating your account and deploying the Smart Contract Database in Polygon Mumbai!
+                </Typography>
+                <Typography id="transition-modal-description" sx={{ mt: 2, textAlign: "center" }}>
+                  This could take between <strong> 15 to 30 seconds</strong> depending on the network congestion. Please <strong>don’t close the window.</strong>
+                </Typography>
+                <Loader/>
+              </Box>
+            </Fade>
+          </Modal>
+          <Footer contractAddress={contractAddress} />
+        </WagmiConfig>
+      </div>
+    </ApolloProvider>
+  );
+}
